9d044d70771021eef7a4b2eda63fb1d3025218b0
Jacob Darvin <jacobisdarvin@gmail.com>
2024-10-04 15:21:41 +0800

    Added logout, destroys localstore user session token

--- src/components/ChatHeader.vue
+++ src/components/ChatHeader.vue
@@ -1,11 +1,14 @@
 <template>
-  <header class="p-4 flex items-center border-b border-neutral-300">
+  <header class="p-4 flex items-center border-b border-neutral-300 justify-between">
     <h2 class="text-2xl">{{ title }}</h2>
+    <button @click="logout">Logout</button>
   </header>
 </template>
 
 <script lang="ts">
+import { useUserStore } from '@/stores/user'
 import { defineComponent } from 'vue'
+import { useRouter } from 'vue-router'
 
 export default defineComponent({
   name: 'ChatHeader',
@@ -14,6 +17,17 @@ export default defineComponent({
       type: String,
       default: 'sk-chat-v2'
     }
+  },
+  setup() {
+    const userStore = useUserStore()
+    const router = useRouter()
+
+    const logout = () => {
+      userStore.logout()
+      router.push('/')
+    }
+
+    return { logout }
   }
 })
 </script>
